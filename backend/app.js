d49850112ae546836ee07a67b8ed190ffc813182
Melanie C <melanie.chedhomme96@gmail.com>
2023-11-21 15:38:19 +0100

    connexion mongo réussi + création schéma dossier model

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,9 @@
+const Book = require('./models/books');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb+srv://<UserTest>:<FyqSCbpVX2I1UyVp>@cluster0.vznqtut.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true, 
+mongoose.connect('mongodb+srv://UserTest:fWTRlpNeRnv77wXH@cluster0.vznqtut.mongodb.net/?retryWrites=true&w=majority', {
 })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((err) => console.error('Connexion à MongoDB échouée !', err));
@@ -24,63 +22,33 @@ app.get('/', (req, res) => {
     res.send('Hello, this is the root path!');
   });
 
-  app.post('api/auth/signup ', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      email: 'monvieuxgrimoires@gmail.com',
-      password: 'P7monvieuxgrimoires'
-    });
-  });
+//   app.post('api/auth/signup ', (req, res, next) => {
+//     console.log(req.body);
+//     res.status(201).json({
+//       email: 'monvieuxgrimoires@gmail.com',
+//       password: 'P7monvieuxgrimoires'
+//     });
+//   });
 
-  app.post('/api/books', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
-  });
+// app.post('/api/books', (req, res, next) => {
+//     delete req.body._id;
+//     const thing = new Book({
+//       ...req.body
+//     });
+//     thing.save()
+//       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+//       .catch(error => res.status(400).json({ error }));
+//   });
 
-app.get('/api/books', (req, res, next) => {
-    const books = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Le seigneur des anneaux',
-        author: 'tolkein',
-        imageUrl: 'https://www.albumcomics.com/114999-large_default/le-seigneur-des-anneaux.jpg',
-        year: 4900,
-        genre:'fantastique',
-        averageRating: 1,
-      },
-      {
-        _id: 'dsfdsfs',
-        title: 'Le seigneur des anneaux',
-        author: 'tolkein',
-        imageUrl: 'https://www.albumcomics.com/114999-large_default/le-seigneur-des-anneaux.jpg',
-        year: 4900,
-        genre:'fantastique',
-        averageRating: 1,
-      },
-      {
-        _id: 'dsffs',
-        title: 'Le seigneur des anneaux',
-        author: 'tolkein',
-        imageUrl: 'https://www.albumcomics.com/114999-large_default/le-seigneur-des-anneaux.jpg',
-        year: 4900,
-        genre:'fantastique',
-        averageRating: 1,
-      },
-      {
-        _id: 'fdfsgs',
-        title: 'Le seigneur des anneaux',
-        author: 'tolkein',
-        imageUrl: 'https://www.albumcomics.com/114999-large_default/le-seigneur-des-anneaux.jpg',
-        year: 4900,
-        genre:'fantastique',
-        averageRating: 1,
-      }
-    ];
-
-   res.status(200).json(books);
-  });
- 
+//   app.get('/api/books', (req, res, next) => {
+//     Book.find()
+//       .then(books => {
+//         if (books.length === 0) {
+//           return res.status(404).json({ message: 'No books found.' });
+//         }
+//         res.status(200).json(books);
+//       })
+//       .catch(error => res.status(500).json({ error: error.message }));
+//   });
 
 module.exports = app;
---
+++ backend/models/books.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  genre: { type: String, required: true },
+  year: { type: Number, required: true },
+  averageRating: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
