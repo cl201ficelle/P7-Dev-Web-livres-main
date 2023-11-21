6fd97bcb2181d8ebc6877c0e87d00bef49d7e2af
Melanie C <melanie.chedhomme96@gmail.com>
2023-11-21 16:32:08 +0100

    création fichier route

--- backend/app.js
+++ backend/app.js
@@ -8,65 +8,68 @@ mongoose.connect('mongodb+srv://UserTest:fWTRlpNeRnv77wXH@cluster0.vznqtut.mongo
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((err) => console.error('Connexion à MongoDB échouée !', err));
 
+  const bookRoutes = require('./routes/route');
+  app.use('/api/books', bookRoutes);
+
 // express prend toutes les requêtes qui ont comme Content-Type  application/json  et met à disposition leur  body  directement sur l'objet req :
-app.use(express.json()); 
+// app.use(express.json()); 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
-app.get('/', (req, res) => {
-    res.send('Hello, this is the root path!');
-  });
-
-//   app.post('api/auth/signup ', (req, res, next) => {
-//     console.log(req.body);
-//     res.status(201).json({
-//       email: 'monvieuxgrimoires@gmail.com',
-//       password: 'P7monvieuxgrimoires'
-//     });
+// app.get('/', (req, res) => {
+//     res.send('Hello, this is the root path!');
 //   });
 
-// app.post('/api/books', (req, res, next) => {
-//     delete req.body._id;
-//     const thing = new Book({
-//       ...req.body
-//     });
-//     thing.save()
-//       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+// //   app.post('api/auth/signup ', (req, res, next) => {
+// //     console.log(req.body);
+// //     res.status(201).json({
+// //       email: 'monvieuxgrimoires@gmail.com',
+// //       password: 'P7monvieuxgrimoires'
+// //     });
+// //   });
+
+// // app.post('/api/books', (req, res, next) => {
+// //     delete req.body._id;
+// //     const thing = new Book({
+// //       ...req.body
+// //     });
+// //     thing.save()
+// //       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+// //       .catch(error => res.status(400).json({ error }));
+// //   });
+
+//   app.get('/api/books', (req, res, next) => {
+//     Book.find()
+//       .then(books => {
+//         if (books.length === 0) {
+//           return res.status(404).json({ message: 'No books found.' });
+//         }
+//         res.status(200).json(books);
+//       })
+//       .catch(error => res.status(500).json({ error: error.message }));
+//   });
+//   app.get('/api/books/:id', (req, res, next) => {
+//     Book.findOne({ _id: req.params.id })
+//       .then(book => res.status(200).json(book))
+//       .catch(error => res.status(404).json({ error }));
+//   });
+
+//   app.put('/api/books/:id', (req, res, next) => {
+//     Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+//       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
 //       .catch(error => res.status(400).json({ error }));
 //   });
 
-  app.get('/api/books', (req, res, next) => {
-    Book.find()
-      .then(books => {
-        if (books.length === 0) {
-          return res.status(404).json({ message: 'No books found.' });
-        }
-        res.status(200).json(books);
-      })
-      .catch(error => res.status(500).json({ error: error.message }));
-  });
-  app.get('/api/books/:id', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-      .then(book => res.status(200).json(book))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-  app.put('/api/books/:id', (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  app.delete('/api/books/:id', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+//   app.delete('/api/books/:id', (req, res, next) => {
+//     Book.deleteOne({ _id: req.params.id })
+//       .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+//       .catch(error => res.status(400).json({ error }));
+//   });
 
 
 module.exports = app;
