1a2bc7fd27690fe066a252a5faa466f5562e9dc9
Melanie C <melanie.chedhomme96@gmail.com>
2023-12-05 14:46:30 +0100

    delete supprime book  + son image

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book');
+const fs = require('fs');
 
 exports.createBook = (req, res, next) => {
    console.log('Requête reçue. Données du body :', req.body);
@@ -56,19 +57,22 @@ exports.modifyBook = (req, res, next) => {
 };
 
 exports.deleteBook = (req, res, next) => {
-  Book.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Book Deleted!'
+  Book.findOne({ _id: req.params.id})
+      .then(book => {
+          if (book.userId != req.auth.userId) {
+              res.status(401).json({message: 'Not authorized'});
+          } else {
+              const filename = book.imageUrl.split('/images/')[1];
+              fs.unlink(`images/${filename}`, () => {
+                  Book.deleteOne({_id: req.params.id})
+                      .then(() => { res.status(200).json({message: 'Livre supprimé !'})})
+                      .catch(error => res.status(401).json({ error }));
+              });
+          }
+      })
+      .catch( error => {
+          res.status(500).json({ error });
       });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
 };
 
 exports.getAllBooks = (req, res, next) => {
