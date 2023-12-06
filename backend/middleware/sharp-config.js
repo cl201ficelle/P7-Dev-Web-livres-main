12e93ff9e6af0cd8dfc962f788dfbefe376bd1df
Melanie C <melanie.chedhomme96@gmail.com>
2023-12-06 13:51:32 +0100

    optimiseImage

--- backend/middleware/sharp-config.js
+++ backend/middleware/sharp-config.js
@@ -1,10 +1,10 @@
 const sharp = require('sharp');
 
-const optimizeImage = (inputPath, outputPath, width, height, format, callback) => {
+const optimiseImage = (inputPath, outputPath, width, height, format, callback) => {
   sharp(inputPath)
     .resize(width, height)
     .toFormat(format)
     .toFile(outputPath, callback);
 };
 
-module.exports = optimizeImage;
+module.exports = optimiseImage;
